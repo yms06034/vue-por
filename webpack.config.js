dd38bdfec7cbcf8cbbd11534da6a747f9cec972a
Kim Sung Jin <yms06034@gmail.com>
2023-04-05 18:39:29 +0900

    Change

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
-// 현재 프로젝트에서 모듈 경로를 찾을 수 있도록 지정.
-// 특히 Windows에서 발생하는 오류 해결을 위한 코드.
-// 이 코드가 없어도 잘 동작하는 경우 필요치 않음.
 const _require = id => require(require.resolve(id, { paths: [require.main.path] }))
 
-// path: NodeJS에서 파일 및 디렉토리 경로 작업을 위한 전역 모듈
 const path = _require('path')
 const HtmlPlugin = _require('html-webpack-plugin')
 const CopyPlugin = _require('copy-webpack-plugin')
@@ -11,27 +7,21 @@ const { VueLoaderPlugin } = _require('vue-loader')
 
 module.exports = {
   resolve: {
-    // 경로에서 확장자 생략 설정
     extensions: ['.js', '.vue'],
-    // 경로 별칭 설정
     alias: {
       '~': path.resolve(__dirname, 'src'),
       'assets': path.resolve(__dirname, 'src/assets')
     }
   },
 
-  // 파일을 읽어들이기 시작하는 진입점 설정
   entry: './src/main.js',
 
-  // 결과물(번들)을 반환하는 설정
+
   output: {
-    // 주석은 기본값!, `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
-    // path: path.resolve(__dirname, 'dist'),
-    // filename: 'main.js',
     clean: true
   },
 
-  // 모듈 처리 방식을 설정
+
   module: {
     rules: [
       {
@@ -41,7 +31,7 @@ module.exports = {
       {
         test: /\.s?css$/,
         use: [
-          // 순서 중요!
+
           'vue-style-loader',
           'style-loader',
           'css-loader',
@@ -51,7 +41,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/, // 제외할 경로
+        exclude: /node_modules/, 
         use: [
           'babel-loader'
         ]
@@ -63,7 +53,6 @@ module.exports = {
     ]
   },
 
-  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
       template: './index.html'
